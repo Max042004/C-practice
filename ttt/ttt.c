7b2259f65444460f42824ffd73e969be8870447f
Max042004 <sau525@gmail.com>
2025-09-17 11:43:15 +0800

    [ttt] Single one unsigned int to store all state Unsigned int has 32 bits. Using first 9 bits for O board, 10-18 bits for X board, 19-22 bits for total move counting, 23-26 bits for board offset.

--- ttt/ttt.c
+++ ttt/ttt.c
@@ -1,8 +1,8 @@
 /*
  A bit-encoding tic-tac-toe
  Using integer value to encoding every state while playing ttt.
- The first 9 bit records X position,
- and 10~18 bit records O position.
+ The first 9 bit records O position,
+ and 10~18 bit records X position.
  */
 
 #include <stdio.h>
@@ -11,16 +11,24 @@
 #include <stdbool.h>
 #include <unistd.h>
 
-/* board 3*3 O/X
+/* board 3*3 X/O
    012
    345
    678
+
+   corresponding unsigned int in bits:
+   0bxxxxxxxxxxxxxxxxxxxxxxxxxx
+     |__||__||_______||_______|
+      4    4     9        9
+      |    |     |        |
+      v    v     v        v
+   offset count  X        O
 */
 
-bool check_win(int *board, char *winner, char *cur_player)
+bool check_win(unsigned int *state)
 {
-	int offset = *cur_player == 79?9:0;
-	int check_board = *board >> offset;
+	// right shift offset for checking current player's board
+	unsigned int check_board = *state >> (*state >> 22);
 
 	//check is current player achieve any line
 	if((check_board & 0b111) == 0b111 |
@@ -30,36 +38,34 @@ bool check_win(int *board, char *winner, char *cur_player)
 	   (check_board & 0b10010010) == 0b10010010 |
 	   (check_board & 0b100100100) == 0b100100100 |
 	   (check_board & 0b100010001) == 0b100010001 |
-	   (check_board & 0b1010100) == 0b1010100){
-		*winner = *cur_player;
+	   (check_board & 0b1010100) == 0b1010100 ) {
 		return true;
 	}
 	return false;
 }
 
 // O first
-void play_game(int *board, char *cur_player)
+void play_game(unsigned int *state)
 {
-	int offset = *cur_player == 79?9:0;
-	int shift, move = 0x1;
-	int X_board = *board & 0x000001FF, O_board = *board & 0x0003FE00;
-	int curr_board = X_board | O_board >> 9;
+	unsigned int shift, move = 0x1;
+	unsigned int O_board = *state & 0x000001FF, X_board = *state & 0x0003FE00;
+	unsigned int curr_board = O_board | X_board >> 9;
 	// if random choose position is repeating, choose again.
 	for(;;){
 		shift = rand() % 9;
 		if((move << shift | curr_board) != curr_board) break;
 	}
-	*board |= move << (shift+offset);
+	*state |= move << (shift+(*state >> 22));
 }
 
-void draw_board(int *board)
+void draw_board(unsigned int *state)
 {
-	int move = 0x1;
+	unsigned int move = 0x1, board = *state & 0x3FFFF;
 	for(int i = 0; i < 9; i++){
 		if((i+1)%3 == 1) printf("|");
 		char piece = ' ';
-		if((*board & move << i) == (move << i)) piece = 88;
-		if((*board & move << (i+9)) == (move << (i+9))) piece = 79;
+		if((board & move << i) == (move << i)) piece = 79;
+		if((board & move << (i+9)) == (move << (i+9))) piece = 88;
 		printf("%c|", piece);
 		if((i+1)%3 == 0) printf("\n");	
 	}
@@ -70,23 +76,25 @@ int main()
 {
 	time_t t;
 	srand((unsigned) time(&t));
-	int board = 0, count = 0;
-	char cur_player = 'O';
-	char winner;
+
+	unsigned int state = 0;
+	
 	for(;;){
-		play_game(&board, &cur_player);
-		draw_board(&board);
-		if(check_win(&board, &winner, &cur_player)) break;
-		if(cur_player == 79) cur_player = 88;
-		else cur_player = 79;
-		count++;
-		if(count > 8){
+		play_game(&state);
+		draw_board(&state);
+		if(check_win(&state)) break;
+
+		/* two states :0 = 0000, 88 = 1001
+		   to transform between two states, using xor */
+		state ^= 0x2400000;
+
+		state += 262144; // count++, meaning state 19's bit++
+		if(((state & 0x3C000) >> 18) > 8){
 			printf("No one win\n");
 			return 0;
 		}
 		sleep(1);
 	}
-	printf("winner: %c\n", winner);
-
+	printf("winner: %c\n", (state >> 22)+79);
 	return 0;		
 }
